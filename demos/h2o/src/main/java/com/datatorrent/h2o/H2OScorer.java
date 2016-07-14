5771fc829047a1d26e2dee10938d4ba1c8014f36
yogidevendra <yogidevendra@apache.org>
2016-07-14 11:24:12 +0530

    Temp checkin2

--- demos/h2o/src/main/java/com/datatorrent/h2o/H2OScorer.java
+++ demos/h2o/src/main/java/com/datatorrent/h2o/H2OScorer.java
@@ -21,75 +21,87 @@ import java.util.Map;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+import hex.genmodel.GenModel;
+import hex.genmodel.easy.EasyPredictModelWrapper;
+import hex.genmodel.easy.RowData;
+
 import com.datatorrent.api.Context;
 import com.datatorrent.api.DefaultInputPort;
 import com.datatorrent.api.DefaultOutputPort;
 
 import com.datatorrent.common.util.BaseOperator;
+import com.datatorrent.netlet.util.DTThrowable;
 
-public class H2OScorer extends BaseOperator
-{
-  private transient gbm_87813ed0_6261_430f_8465_d34a11fa6f31 model;
-  private long[] cyclesCount = {0, 0, 0, 0}; // 0-50 need urgent care, 50-100, 100-150, 150+
+public class H2OScorer extends BaseOperator {
+	private static String modelClassName = "glm_f25f6cc8_3665_49dc_82c1_eaf0be1dd391";
+	private transient EasyPredictModelWrapper model;
 
-  public final transient DefaultOutputPort<List<Map<String, Object>>> output = new DefaultOutputPort<>();
-  public final transient DefaultInputPort<String> input = new DefaultInputPort<String>()
-  {
-    @Override
-    public void process(String s)
-    {
-      String[] splits = s.split(",");
-      double[] data = new double[splits.length - 17];
-      for (int i = 17; i < splits.length; i++) {
-        data[i - 17] = Double.parseDouble(splits[i]);
-      }
-      double[] pred = new double[1];
-      model.score0(data, pred);
-      double absPred = Math.abs(pred[0]);
-      if (absPred <= 50) {
-        cyclesCount[0]++;
-      }
-      else if (absPred <= 100) {
-        cyclesCount[1]++;
-      }
-      else if (absPred < 150) {
-        cyclesCount[2]++;
-      }
-      else {
-        cyclesCount[3]++;
-      }
-    }
-  };
+	public final transient DefaultOutputPort<List<Map<String, Object>>> output = new DefaultOutputPort<>();
+	public final transient DefaultInputPort<String> input = new DefaultInputPort<String>() {
+		@Override
+		public void process(String s) {
 
+			RowData row = new RowData();
+			row.put("Year", "1987");
+			row.put("Month", "10");
+			row.put("DayofMonth", "14");
+			row.put("DayOfWeek", "3");
+			row.put("CRSDepTime", "730");
+			row.put("UniqueCarrier", "PS");
+			row.put("Origin", "SAN");
+			row.put("Dest", "SFO");
 
-  @Override
-  public void endWindow()
-  {
-    super.endWindow();
-    List<Map<String, Object>> results = Lists.newArrayList();
-    Map<String, Object> resultMap = Maps.newHashMap();
-    resultMap.put("Remaining Cycles", "[0-50] cycles");
-    resultMap.put("count", cyclesCount[0]);
-    results.add(resultMap);
-    resultMap = Maps.newHashMap();
-    resultMap.put("Remaining Cycles", "(50-100] cycles");
-    resultMap.put("count", cyclesCount[1]);
-    results.add(resultMap);
-    resultMap = Maps.newHashMap();
-    resultMap.put("Remaining Cycles", "(100-150] cycles");
-    resultMap.put("count", cyclesCount[2]);
-    results.add(resultMap);
-    resultMap = Maps.newHashMap();
-    resultMap.put("Remaining Cycles", "(150- ] cycles");
-    resultMap.put("count", cyclesCount[3]);
-    results.add(resultMap);
-    output.emit(results);
-  }
+			String[] splits = s.split(",");
+			double[] data = new double[splits.length - 17];
+			for (int i = 17; i < splits.length; i++) {
+				data[i - 17] = Double.parseDouble(splits[i]);
+			}
+			double[] pred = new double[1];
+			model.score0(data, pred);
+			double absPred = Math.abs(pred[0]);
+			if (absPred <= 50) {
+				cyclesCount[0]++;
+			} else if (absPred <= 100) {
+				cyclesCount[1]++;
+			} else if (absPred < 150) {
+				cyclesCount[2]++;
+			} else {
+				cyclesCount[3]++;
+			}
+		}
+	};
 
-  @Override
-  public void setup(Context.OperatorContext context)
-  {
-    super.setup(context);
-    model = new gbm_87813ed0_6261_430f_8465_d34a11fa6f31();
-  }
+	@Override
+	public void endWindow() {
+		super.endWindow();
+		List<Map<String, Object>> results = Lists.newArrayList();
+		Map<String, Object> resultMap = Maps.newHashMap();
+		resultMap.put("Remaining Cycles", "[0-50] cycles");
+		resultMap.put("count", cyclesCount[0]);
+		results.add(resultMap);
+		resultMap = Maps.newHashMap();
+		resultMap.put("Remaining Cycles", "(50-100] cycles");
+		resultMap.put("count", cyclesCount[1]);
+		results.add(resultMap);
+		resultMap = Maps.newHashMap();
+		resultMap.put("Remaining Cycles", "(100-150] cycles");
+		resultMap.put("count", cyclesCount[2]);
+		results.add(resultMap);
+		resultMap = Maps.newHashMap();
+		resultMap.put("Remaining Cycles", "(150- ] cycles");
+		resultMap.put("count", cyclesCount[3]);
+		results.add(resultMap);
+		output.emit(results);
+	}
+
+	@Override
+	public void setup(Context.OperatorContext context) {
+		super.setup(context);
+		try {
+			GenModel rawModel = (hex.genmodel.GenModel) Class.forName(modelClassName).newInstance();
+			model = new EasyPredictModelWrapper(rawModel);
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			DTThrowable.rethrow(e);
+		}
+	}
 }
